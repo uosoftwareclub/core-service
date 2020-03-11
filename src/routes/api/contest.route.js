c0f6c742c97ef7e96b24dfbbe4e74706852eb9bc
David Hew-wing <dhew67@gmail.com>
2020-03-11 16:47:34 -0400

    removed random console.log

--- src/routes/api/contest.route.js
+++ src/routes/api/contest.route.js
@@ -4,8 +4,6 @@ const auth = require('../../middlewares/authorization')
 
 const router = express.Router()
 
-console.log("ALSKD")
-
 router.get('/status', auth, contestController.status);
 
 module.exports = router;
