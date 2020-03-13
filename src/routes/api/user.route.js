c3727aec49b2a998d617487e59d6b450a683bc3a
David Hew-wing <dhew67@gmail.com>
2020-03-13 00:12:44 -0400

    forgot auth middleware

--- src/routes/api/user.route.js
+++ src/routes/api/user.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const userController = require('../../controllers/user.controller');
 const { userValidationRules, validate } = require('../../validation/user.validation');
+const auth = require('../../middlewares/authorization')
 
 const router = express.Router();
 
 router.get('/status', userController.status);
 
-router.post('/', userValidationRules(), validate, userController.add_user);
+router.post('/', userValidationRules(), validate, auth, userController.add_user);
 
 module.exports = router;
