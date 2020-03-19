a28f0821741818e31f998944fe998a719f0709d7
Yong Lin <wangyonglin1999@gmail.com>
2020-03-18 20:40:22 -0400

    chore: remove log + add new destination

--- src/services/express.js
+++ src/services/express.js
@@ -16,8 +16,6 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(helmet());
 
-console.log(config.env);
-
 if (config.env !== 'test') app.use(morgan('combined', { stream: logger.stream }));
 
 app.use(`/${apiVersion}`, apiRouter);
