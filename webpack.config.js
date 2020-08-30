ec3b9eaa658f57f71ea2f114d9e1353384497481
Yong Lin <wangyonglin1999@gmail.com>
2020-08-30 12:42:48 -0400

    feat: added webpack to bundle our core services

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const webpack = require('webpack');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: {
+    server: './src/index.js',
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    filename: '[name].js',
+  },
+  target: 'node',
+  node: {
+    // Need this when working with express, otherwise the build fails
+    __dirname: false, // if you don't put this is, __dirname
+    __filename: false, // and __filename return blank or /
+  },
+  externals: [nodeExternals()],
+};
